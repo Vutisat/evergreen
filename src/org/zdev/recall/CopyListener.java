00f61155acfea8de3c6c284e8e029a8df1d9e4f3
Matt Anderson <andersonmat42@gmail.com>
2013-10-21 21:35:26 -0400

    Updated to properly retrieve data off of the clipboard (in CopyListener.class) and got around a current 4.x bug in Android.
    Settings are now visible -- yet that is all filler data, I have not added any actual configuration information.
    
    I added an ArrayList we can use to store elements that we want to have in memory and the CopyListener.class populates it when new items are copied.

--- src/org/zdev/recall/CopyListener.java
+++ src/org/zdev/recall/CopyListener.java
@@ -1,16 +1,17 @@
 package org.zdev.recall;
 
-import android.app.Activity;
 import android.content.ClipboardManager;
 import android.content.ClipboardManager.OnPrimaryClipChangedListener;
+import android.text.SpannableString;
+import android.util.Log;
 import android.widget.Toast;
 
 public class CopyListener implements OnPrimaryClipChangedListener {
 
-	private Activity parentInstance;
+	private MainActivity parentInstance;
 	private ClipboardManager clipboardManager;
 
-	CopyListener(Activity parentInstance, ClipboardManager clipboardManager) {
+	CopyListener(MainActivity parentInstance, ClipboardManager clipboardManager) {
 		this.parentInstance = parentInstance;
 		this.clipboardManager = clipboardManager;
 	}
@@ -18,11 +19,31 @@ public class CopyListener implements OnPrimaryClipChangedListener {
 	@Override
 	public void onPrimaryClipChanged() {
 
-		// Get most recent text off Clipboard
-		String newClipping = (String) this.clipboardManager.getPrimaryClip().getItemAt(0).getText();
-		
-		// just debugging
-		Toast.makeText(this.parentInstance, "New Text:" + newClipping, Toast.LENGTH_SHORT).show();
+		// fucking java
+		try {
 
+			/*
+			 * NOTE: This is a really hacky way of going about doing this.
+			 * Apparently this is a bug in 4.x where getText will return an
+			 * instance of SpannedString which cannot be cast to string. So we
+			 * have to create an instance of SpannableString which takes the
+			 * return value as a parameter and then converts it to a string.
+			 */
+
+			// Get the most recent clipping off of the clipboard
+			String newClipping = new SpannableString(this.clipboardManager.getPrimaryClip().getItemAt(0).getText()).toString();
+			
+			
+			// check for a proper length (non-empty string)
+			if(newClipping.length() > 0) {
+				this.parentInstance.addElementToList(newClipping);
+			}
+
+			// just debugging
+			Toast.makeText(this.parentInstance, "New Text:" + newClipping, Toast.LENGTH_SHORT).show();
+
+		} catch (Exception e) {
+			Log.d("err", e.toString());
+		}
 	}
 }
--- src/org/zdev/recall/MainActivity.java
+++ src/org/zdev/recall/MainActivity.java
@@ -1,45 +1,66 @@
 package org.zdev.recall;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.content.ClipboardManager;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity {
+
+	private ArrayList<String> clipboardElements = new ArrayList<String>();
+	
+	
+	public void addElementToList(String aValue) {
+		this.clipboardElements.add(aValue);
+		
+		Log.d("New Element", "Clipboard Length: " + this.clipboardElements.size());
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		/*
-		 * Essentially, this beginning portion is going to be retrieving the
-		 * system's clipboard manager and then adding a listener for when the
-		 * copied text changes so we can store it in memory.
-		 */
-		
-
-
-		// grab the manager and specify our listener
+		// grab the manager
 		ClipboardManager clipboardManager = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
-		
-		// create instance of our copy listener (this will receive events from the ClipboardManager
-		// also pass ClipboardManager instance because of context access ffs
+
+		// create instance of our copy listener
 		CopyListener copyListener = new CopyListener(this, clipboardManager);
-		
+
+		// ...and specify our listener
 		clipboardManager.addPrimaryClipChangedListener(copyListener);
-		
-		
-		// will be useful to show the user that we copied the data
-		// Toast.makeText(this, "Text Copied To Clipboard",
-		// Toast.LENGTH_SHORT).show();
 
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// uncomment this to enable the dropdown
-		// getMenuInflater().inflate(R.menu.main, menu);
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		// let's find out what drop down item we chose
+		switch (item.getItemId()) {
+
+		// if we match this we'll transition to our settings activity
+		case R.id.action_settings:
+
+			// create intent instance wrapping our settings activity
+			Intent settingsIntent = new Intent(this, SettingsActivity.class);
+			startActivityForResult(settingsIntent, 1);
+
+			break;
+
+		}
+
 		return true;
 	}
 
