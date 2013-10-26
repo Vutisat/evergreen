1556e3f04b52945445d4f91dd767dbfc71ccfc25
Matt Anderson <andersonmat42@gmail.com>
2013-10-26 16:08:39 -0400

    Slowing transitioning to a new ClippedItem class for storing clipboard data. This will allow us to "pin" items.

---
+++ src/org/zdev/recall/ClippedItem.java
@@ -0,0 +1,32 @@
+package org.zdev.recall;
+
+public class ClippedItem {
+
+	private boolean pinnedClipping;
+	private String clippingContents;
+
+	ClippedItem(String clippingContents) {
+		this.clippingContents = clippingContents;
+	}
+
+	public void pin() {
+		this.pinnedClipping = true;
+	}
+
+	public void unpin() {
+		this.pinnedClipping = false;
+	}
+
+	public boolean isPinned() {
+		return this.pinnedClipping;
+	}
+
+	public String getContents() {
+		return this.clippingContents;
+	}
+
+	public String toString() {
+		return this.clippingContents;
+	}
+
+}
